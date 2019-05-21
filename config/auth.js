3446042d9dd569e05a77f91687eeb07d4452117a
J Lew <jtl@metricwise.net>
2019-05-20 21:45:04 -0400

    fix lint errors

--- config/auth.js
+++ config/auth.js
@@ -1,20 +1,18 @@
-const GoogleStrategy = require('passport-google-    oauth').OAuth2Strategy;
-module.exports = (passport) => {
-    passport.serializeUser((user, done) => {
-        done(null, user);
-    });
-    passport.deserializeUser((user, done) => {
-        done(null, user);
-    });
-    passport.use(new GoogleStrategy({
-        clientID: % your_client_ID %,
-        clientSecret: % your_client_secret %,
-        callbackURL: % your_callback_url %
-        },
-        (token, refreshToken, profile, done) => {
-            return done(null, {
-                profile: profile,
-                token: token
-            });
-        }));
-};
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth").OAuthStrategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and Google profile), and
+//   invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+  consumerKey: "1016489823423-8q3qleq5ns0bkhh0lrcgnj4kqs4rc1pd.apps.googleusercontent.com",
+  consumerSecret: "ziLWufLTn5sS2Xje9k_aztT8",
+  callbackURL: "https://bnhproject2.herokuapp.com/#/"
+},
+function (token, tokenSecret, profile, done) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return done(err, user);
+  });
+}
+));
