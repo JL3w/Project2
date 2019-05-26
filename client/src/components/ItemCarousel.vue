2a8789ef7459fba3d5c77fac1a8dbe6478be52f4
Paul Nelson <pmnelso@gmail.com>
2019-05-26 09:23:11 -0400

    Created new view and component files

---
+++ client/src/components/ItemCarousel.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container" id="itemCarousel">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ItemCarousel"
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ client/src/components/RatingStars.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container" id="ratingStars">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RatingStars"
+}
+</script>
+
+<style scoped>
+    
+</style>
