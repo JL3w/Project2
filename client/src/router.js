4c271daebd1f233b5c79ea14484cfd3d529765af
J Lew <jtl@metricwise.net>
2019-06-01 13:36:30 -0400

    add Home route

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Register from '@/components/Register';
+import Home from '@/components/Home';
 import Login from '@/components/Login';
 import Loadouts from '@/components/Loadouts';
 import CreateLoadout from '@/components/CreateLoadout';
