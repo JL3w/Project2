a09c233d83e9844c7d4202c25f1ddf45af50dea8
J Lew <jtl@metricwise.net>
2019-06-03 10:07:57 -0400

    add static rows to loadout

--- client/src/components/CreateLoadout.vue
+++ client/src/components/CreateLoadout.vue
@@ -2,6 +2,17 @@
     <v-layout column>
         <v-flex>
             <br>
+            <v-btn class="light-green accent-4 mb-3 mr-3" 
+                slot="action"
+                @click="addField" 
+                dark 
+                fab
+                fixed
+                bottom
+                right
+                large >
+                <v-icon>add</v-icon>
+            </v-btn>
             <Panel title="New Loadout">
                 <br>
                 <v-text-field light
@@ -20,19 +31,61 @@
                     solo
                 ></v-text-field>
                 <v-text-field light
-                    v-model="loadout.items.name"
+                    v-model="loadout.items.item[0].name"
                     placeholder="Item"
                     solo
                 >
                 </v-text-field>
                 <v-text-field light
-                    v-model="loadout.items.imageURL"
+                    v-model="loadout.items.item[0].imageURL"
                     placeholder="Image URL"
                     solo
                 ></v-text-field>
-
-                <v-btn class="deep-orange darken-3" @click="create" dark>Create</v-btn>
-
+                <v-text-field light
+                    v-model="loadout.items.item[1].name"
+                    placeholder="Item"
+                    solo
+                >
+                </v-text-field>
+                <v-text-field light
+                    v-model="loadout.items.item[1].imageURL"
+                    placeholder="Image URL"
+                    solo
+                ></v-text-field>
+                <v-btn class="deep-orange darken-3" @click="addRow" dark>Create</v-btn>
+                <v-text-field light
+                    v-model="loadout.items.item[2].name"
+                    placeholder="Item"
+                    solo
+                >
+                </v-text-field>
+                <v-text-field light
+                    v-model="loadout.items.item[2].imageURL"
+                    placeholder="Image URL"
+                    solo
+                ></v-text-field>
+                <v-text-field light
+                    v-model="loadout.items.item[3].name"
+                    placeholder="Item"
+                    solo
+                >
+                </v-text-field>
+                <v-text-field light
+                    v-model="loadout.items.item[3].imageURL"
+                    placeholder="Image URL"
+                    solo
+                ></v-text-field>
+                <v-text-field light
+                    v-model="loadout.items.item[4].name"
+                    placeholder="Item"
+                    solo
+                >
+                </v-text-field>
+                <v-text-field light
+                    v-model="loadout.items.item[4].imageURL"
+                    placeholder="Image URL"
+                    solo
+                ></v-text-field>
             </Panel>
         </v-flex>
     </v-layout>
@@ -49,16 +102,35 @@ export default {
                 activity: null,
                 description: null,
                 items: {
-                    name: null,
-                    imageURL: null,
+                    item: {
+                        name: null,
+                        imageURL: null,
+                    },
+                    item: {
+                        name: null,
+                        imageURL: null,
+                    },
+                    item: {
+                        name: null,
+                        imageURL: null,
+                    },
+                    item: {
+                        name: null,
+                        imageURL: null,
+                    },
+                    item: {
+                        name: null,
+                        imageURL: null,
+                    }
+
                 }
             }
         }
     },
     methods: {
-        create () {
+        create() {
             // API code
-        }
+        },
     },
     components: {
         Panel
